a6b08ecba139ae8428a7dbf1bc9a97254bef0dac
Jacob Birkett <jacob@birkett.dev>
2023-11-12 00:46:18 -0700

    copy slint example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    dbg!(env!("CARGO_MANIFEST_DIR"));
+
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    ui.on_request_counter_add(move |amount: i32| {
+        let ui = ui_handle.unwrap();
+        ui.set_counter(ui.get_counter() + amount);
+    });
+
+    ui.run()
 }
